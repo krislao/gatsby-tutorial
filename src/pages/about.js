0dae84e504423eec307c7de6db118f94e0ae8f05
krislao <krislao13@gmail.com>
2021-04-10 13:04:30 +0800

    Create sample pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{ color: "green" }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  );
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from "react";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: "purple" }}>
+      <h1>Hello Kris Lao!</h1>
+      <p>A sample paragraph in Gatsby</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
